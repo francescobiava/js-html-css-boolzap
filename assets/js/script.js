e035f4e641e1a384241818122ea16c60c7caff90
Francesco <franc.ns@gmail.com>
2019-11-12 00:19:29 +0100

    added press enter to send message and control to send only if the input is not empty

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,32 @@ $(document).ready(function () {
 
 // FUNCTIONS
 function sendMessage() {
-  $('#send-message').click(function() { 
-    // recupero il messaggio scritto dall'utente
-    var message = $('.type-message input').val();
-    // inserisco il messaggio nella copia presa dal template
-    var liMessage = $('#template .sent-message').clone();
-    liMessage.find('span').text(message);
-    // appendo dove mi serve
-    $('.conversation ul').append(liMessage);
-    // svuoto l'input
-    $('.type-message input').val('');
+  $('#send-message').click(function() {
+    if ($('.type-message input').val() !== '') { 
+      // recupero il messaggio scritto dall'utente
+      var message = $('.type-message input').val();
+      // inserisco il messaggio nella copia presa dal template
+      var liMessage = $('#template .sent-message').clone();
+      liMessage.find('span').text(message);
+      // appendo dove mi serve
+      $('.conversation ul').append(liMessage);
+      // svuoto l'input
+      $('.type-message input').val('');
+    }
+  });
+  $(document).on('keydown',function(e) {
+    if(e.which == 13) {
+      if ($('.type-message input').val() !== '') { 
+        // recupero il messaggio scritto dall'utente
+        var message = $('.type-message input').val();
+        // inserisco il messaggio nella copia presa dal template
+        var liMessage = $('#template .sent-message').clone();
+        liMessage.find('span').text(message);
+        // appendo dove mi serve
+        $('.conversation ul').append(liMessage);
+        // svuoto l'input
+        $('.type-message input').val('');
+      }  
+    }
   });
 }
